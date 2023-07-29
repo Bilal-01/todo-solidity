31c15a4a04a730f4ed8d601981cb563bafa800b8
Bilal-01 <mbilalaziz.01@gmail.com>
2023-07-29 20:32:42 +0500

    welcome page completed and navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-content justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
-                <img src = {logo} alt="logo" className="w-32 cursor-pointer" /> 
+                <img src = {logo} alt="logo" className="w-32 cursor-pointer ml-auto" /> 
             </div>
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                 {
