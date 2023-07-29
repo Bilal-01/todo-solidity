73365856375f9838b0135b67cf4ac53772589ac4
Bilal-01 <mbilalaziz.01@gmail.com>
2023-07-29 21:31:17 +0500

    smart contract set up

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+contract Transactions {
+    uint256 transactionCounter;
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBloackchain() public {
+
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        
+    }
+
+
+}
